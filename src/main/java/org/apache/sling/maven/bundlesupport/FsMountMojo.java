c58cbfd2620097deb47f057a0bdb505bb9d28379
sseifert <sseifert@pro-vision.de>
2018-05-03 08:51:36 +0200

    SLING-7645 Maven Sling Plugin: Update to latest fsresource

--- src/main/java/org/apache/sling/maven/bundlesupport/FsMountMojo.java
+++ src/main/java/org/apache/sling/maven/bundlesupport/FsMountMojo.java
@@ -59,7 +59,7 @@ public class FsMountMojo extends AbstractFsMountMojo {
     private static final String BUNDLE_GROUP_ID = "org.apache.sling"; 
 
     private static final String FS_BUNDLE_ARTIFACT_ID = "org.apache.sling.fsresource"; 
-    private static final String FS_BUNDLE_DEFAULT_VERSION = "2.1.12"; 
+    private static final String FS_BUNDLE_DEFAULT_VERSION = "2.1.14"; 
     private static final String FS_BUNDLE_LEGACY_DEFAULT_VERSION = "1.4.8"; 
     
     private static final String RESOURCE_RESOLVER_BUNDLE_ARTIFACT_ID = "org.apache.sling.resourceresolver"; 
@@ -103,7 +103,7 @@ public class FsMountMojo extends AbstractFsMountMojo {
      *       &lt;bundle&gt;
      *         &lt;groupId&gt;org.apache.sling&lt;/groupId&gt;
      *         &lt;artifactId&gt;org.apache.sling.fsresource&lt;/artifactId&gt;
-     *         &lt;version&gt;2.1.12&lt;/version&gt;
+     *         &lt;version&gt;2.1.14&lt;/version&gt;
      *       &lt;/bundle&gt;
      *     &lt;/bundles&gt;
      *     &lt;preconditions&gt;
